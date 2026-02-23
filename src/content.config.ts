b10cd72ae13869c8e60e71278a3888c33435abdc
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2026-02-22 22:26:32 -0500

    chore!: change gears from notion to markdown files

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,53 +1,22 @@
 import { z } from "astro:content";
 import { defineCollection } from "astro:content";
-import { notionLoader } from "@chlorinec-pkgs/notion-astro-loader";
-import {
-  notionPageSchema,
-  propertySchema,
-  transformedPropertySchema,
-} from "@chlorinec-pkgs/notion-astro-loader/schemas";
-import { NOTION_TOKEN, NOTION_DATABASE_ID } from "astro:env/server";
+import { glob } from 'astro/loaders';
+import tags from "./data/tags.json";
 
-const defaultImageUrl = "https://placehold.co/600x500.png"; // when image missing somehow
+const tagKeys = Object.keys(tags) as [string, ...string[]];
 
 const posts = defineCollection({
-  loader: notionLoader({
-    auth: NOTION_TOKEN,
-    database_id: NOTION_DATABASE_ID,
-    filter: {
-      property: "Published",
-      checkbox: { equals: true },
-    },
-    sorts: [{ property: "Date", direction: "descending" }],
-  }),
-  schema: notionPageSchema({
-    properties: z.object({
-      Name: transformedPropertySchema.title,
-      Description: transformedPropertySchema.rich_text,
-      Image: propertySchema.files.transform((files) => {
-        const firstFile = files.files[0];
-        if (!firstFile) return defaultImageUrl;
-        if (firstFile.type === "file") {
-          return firstFile.file.url;
-        } else if (firstFile.type === "external") {
-          return firstFile.external.url;
-        }
-        return defaultImageUrl;
-      }),
-      ImageAlt: transformedPropertySchema.rich_text,
-      Date: transformedPropertySchema.date.transform((property) => {
-        return property?.start;
-      }),
-      Published: transformedPropertySchema.checkbox,
-      Slug: transformedPropertySchema.rich_text.refine(
-        (slug) => /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(slug),
-        {
-          message: "Slug must be lowercase, alphanumeric, and kebab-case",
-        }
-      ),
-      Authors: transformedPropertySchema.multi_select,
-      Tags: transformedPropertySchema.multi_select
-    }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/articles/" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string().optional(),
+    authors: z.array(z.string()),
+    helpers: z.array(z.string()).optional(),
+    tags: z.array(z.enum(tagKeys)).optional(),
+    date: z.date(),
+    published: z.boolean().default(true),
+    slug: z.string(),
   }),
 });
 
